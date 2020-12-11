4bd0b1e634d117d4860f9a38519788119d96b1bb
Dmitriy Maksimov <dmitriy.maksimov@aveva.com>
2020-12-11 15:30:23 +1000

    Added solution files

---
+++ Leetcode/Leetcode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// Catch2
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+using namespace std;
+
+#include "solutions/136_SingleNumber.h"
+
+
+TEST_CASE("Single Number", "[136]")
+{
+}
